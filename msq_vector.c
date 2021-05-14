775988b55639639a6b511a594d8cdffc4ef24f00
musique88 <musique88181@gmail.com>
2021-05-14 14:37:38 -0400

    used typedef

--- msq_vector.c
+++ msq_vector.c
@@ -3,162 +3,162 @@
 
 // v2i
 
-float v2i_length(struct vector2i v)
+float v2i_length(vector2i v)
 {
 	return sqrt(v.x*v.x+ v.y*v.y);
 }
 
-struct vector2i v2i_add(struct vector2i v1, struct vector2i v2)
+vector2i v2i_add(vector2i v1, vector2i v2)
 {
-	return (struct vector2i) {v1.x + v2.x, v1.y + v2.y};
+	return (vector2i) {v1.x + v2.x, v1.y + v2.y};
 }
 
-struct vector2i v2i_mult(struct vector2i v, int k)
+vector2i v2i_mult(vector2i v, int k)
 {
-	return (struct vector2i){v.x * k,v.y * k};
+	return (vector2i){v.x * k,v.y * k};
 }
 
-struct vector2f v2i_to_v2f(struct vector2i v)
+vector2f v2i_to_v2f(vector2i v)
 {
-	return (struct vector2f){v.x,v.y};
+	return (vector2f){v.x,v.y};
 }
 
-struct vector2f v2i_normalised(struct vector2i v)
+vector2f v2i_normalised(vector2i v)
 {
 	float l = v2i_length(v);
-	return (struct vector2f) {(float)v.x / l, (float)v.y / l};
+	return (vector2f) {(float)v.x / l, (float)v.y / l};
 }
 
 // v2f
 
-float v2f_length(struct vector2f v)
+float v2f_length(vector2f v)
 {
 	return sqrt(v.x*v.x+ v.y*v.y);
 }
 
-struct vector2f v2f_add(struct vector2f v1, struct vector2f v2)
+vector2f v2f_add(vector2f v1, vector2f v2)
 {
-	return (struct vector2f) {v1.x + v2.x, v1.y + v2.y};
+	return (vector2f) {v1.x + v2.x, v1.y + v2.y};
 }
 
-struct vector2f v2f_mult(struct vector2f v, float k)
+vector2f v2f_mult(vector2f v, float k)
 {
-	return (struct vector2f){v.x * k,v.y * k};
+	return (vector2f){v.x * k,v.y * k};
 }
 
-struct vector2f v2f_normalised(struct vector2f v)
+vector2f v2f_normalised(vector2f v)
 {
 	float l = v2f_length(v);
-	return (struct vector2f) {v.x / l, v.y / l};
+	return (vector2f) {v.x / l, v.y / l};
 }
 
 
 // v3i
 
-float v3i_length(struct vector3i v)
+float v3i_length(vector3i v)
 {
 	return sqrt( v.x*v.x + v.y*v.y + v.z*v.z);
 }
 
-struct vector3i v3i_add(struct vector3i v1, struct vector3i v2)
+vector3i v3i_add(vector3i v1, vector3i v2)
 {
-	return (struct vector3i) {v1.x + v2.x, v1.y + v2.y, v1.z + v2.z};
+	return (vector3i) {v1.x + v2.x, v1.y + v2.y, v1.z + v2.z};
 }
 
-struct vector3i v3i_mult(struct vector3i v, int k)
+vector3i v3i_mult(vector3i v, int k)
 {
-	return (struct vector3i){v.x * k,v.y * k,v.z * k};
+	return (vector3i){v.x * k,v.y * k,v.z * k};
 }
 
-struct vector3f v3i_to_v3f(struct vector3i v)
+vector3f v3i_to_v3f(vector3i v)
 {
-	return (struct vector3f){v.x,v.y,v.z};
+	return (vector3f){v.x,v.y,v.z};
 }
 
-struct vector3f v3i_normalised(struct vector3i v)
+vector3f v3i_normalised(vector3i v)
 {
 	float l = v3i_length(v);
-	return (struct vector3f) {(float)v.x / l, (float)v.y / l, (float)v.z / l};
+	return (vector3f) {(float)v.x / l, (float)v.y / l, (float)v.z / l};
 }
 
 // v3f
 
-float v3f_length(struct vector3f v)
+float v3f_length(vector3f v)
 {
 	return sqrt( v.x*v.x + v.y*v.y + v.z*v.z);
 }
-struct vector3f v3f_add(struct vector3f v1, struct vector3f v2)
+vector3f v3f_add(vector3f v1, vector3f v2)
 {
-	return (struct vector3f) {v1.x + v2.x, v1.y + v2.y, v1.z + v2.z};
+	return (vector3f) {v1.x + v2.x, v1.y + v2.y, v1.z + v2.z};
 }
 
-struct vector3f v3f_mult(struct vector3f v, float k)
+vector3f v3f_mult(vector3f v, float k)
 {
-	return (struct vector3f){v.x * k,v.y * k,v.z * k};
+	return (vector3f){v.x * k,v.y * k,v.z * k};
 }
 
-struct vector3i v3f_to_v3i(struct vector3f v)
+vector3i v3f_to_v3i(vector3f v)
 {
-	return (struct vector3i){v.x,v.y,v.z};
+	return (vector3i){v.x,v.y,v.z};
 }
 
-struct vector3f v3f_normalised(struct vector3f v)
+vector3f v3f_normalised(vector3f v)
 {
 	float l = v3f_length(v);
-	return (struct vector3f) {v.x / l, v.y / l, v.z / l};
+	return (vector3f) {v.x / l, v.y / l, v.z / l};
 }
 
 // v4i
 
-float v4i_length(struct vector4i v)
+float v4i_length(vector4i v)
 {
 	return sqrt( v.x*v.x + v.y*v.y + v.z*v.z + v.w*v.w);
 }
 
-struct vector4i v4i_add(struct vector4i v1, struct vector4i v2)
+vector4i v4i_add(vector4i v1, vector4i v2)
 {
-	return (struct vector4i) {v1.x + v2.x, v1.y + v2.y, v1.z + v2.z, v1.w + v2.w};
+	return (vector4i) {v1.x + v2.x, v1.y + v2.y, v1.z + v2.z, v1.w + v2.w};
 }
-struct vector4i v4i_mult(struct vector4i v, int k)
+vector4i v4i_mult(vector4i v, int k)
 {
-	return (struct vector4i){v.x * k,v.y * k,v.z * k,v.w * k};
-}
-
-struct vector4f v4i_to_v4f(struct vector4i v)
-{
-	return (struct vector4f){v.x,v.y,v.z,v.w};
+	return (vector4i){v.x * k,v.y * k,v.z * k,v.w * k};
 }
 
-struct vector4f v4i_normalised(struct vector4i v)
+vector4f v4i_to_v4f(vector4i v)
+{
+	return (vector4f){v.x,v.y,v.z,v.w};
+}
+
+vector4f v4i_normalised(vector4i v)
 {
 	float l = v4i_length(v);
-	return (struct vector4f) {(float)v.x / l, (float)v.y / l, (float)v.z / l, (float)v.w / l};
+	return (vector4f) {(float)v.x / l, (float)v.y / l, (float)v.z / l, (float)v.w / l};
 }
 
 // v4f
 
-float v4f_length(struct vector4f v)
+float v4f_length(vector4f v)
 {
 	return sqrt( v.x*v.x + v.y*v.y + v.z*v.z + v.w*v.w);
 }
 
-struct vector4f v4f_add(struct vector4f v1, struct vector4f v2)
+vector4f v4f_add(vector4f v1, vector4f v2)
 {
-	return (struct vector4f) {v1.x + v2.x, v1.y + v2.y, v1.z + v2.z, v1.w + v2.w};
+	return (vector4f) {v1.x + v2.x, v1.y + v2.y, v1.z + v2.z, v1.w + v2.w};
 }
 
-struct vector4f v4f_mult(struct vector4f v, float k)
+vector4f v4f_mult(vector4f v, float k)
 {
-	return (struct vector4f){v.x * k,v.y * k,v.z * k,v.w * k};
+	return (vector4f){v.x * k,v.y * k,v.z * k,v.w * k};
 }
 
-struct vector4i v4f_to_v4i(struct vector4f v)
+vector4i v4f_to_v4i(vector4f v)
 {
-	return (struct vector4i){v.x,v.y,v.z,v.w};
+	return (vector4i){v.x,v.y,v.z,v.w};
 }
 
-struct vector4f v4f_normalised(struct vector4f v)
+vector4f v4f_normalised(vector4f v)
 {
 	float l = v4f_length(v);
-	return (struct vector4f) {v.x / l, v.y / l, v.z / l, v.w / l};
+	return (vector4f) {v.x / l, v.y / l, v.z / l, v.w / l};
 }
--- msq_vector.h
+++ msq_vector.h
@@ -1,65 +1,65 @@
-struct vector2i {
+typedef struct{
 	int x,y;
-};
+} vector2i;
 
-struct vector3i {
+typedef struct {
 	int x,y,z;
-};
+} vector3i;
 
-struct vector4i {
+typedef struct {
 	int x,y,z,w;
-};
+}vector4i;
 
-struct vector2f{
+typedef struct{
 	float x,y;
-};
+}vector2f;
 
-struct vector3f {
+typedef struct {
 	float x,y,z;
-};
+}vector3f;
 
-struct vector4f {
+typedef struct {
 	float x,y,z,w;
-};
+}vector4f;
 
 // v2i
 
-float v2i_length(struct vector2i v);
-struct vector2i v2i_add(struct vector2i v1, struct vector2i v2);
-struct vector2i v2i_mult(struct vector2i v, int k);
-struct vector2f v2i_to_v2f(struct vector2i v);
+float v2i_length(vector2i v);
+vector2i v2i_add(vector2i v1,  vector2i v2);
+vector2i v2i_mult(vector2i v, int k);
+vector2f v2i_to_v2f(vector2i v);
 
 // v2f
 
-float v2f_length(struct vector2f v);
-struct vector2f v2f_add(struct vector2f v1, struct vector2f v2);
-struct vector2f v2f_mult(struct vector2f v, float k);
-struct vector2i v2f_to_v2i(struct vector2f v);
+float v2f_length(vector2f v);
+vector2f v2f_add(vector2f v1, vector2f v2);
+vector2f v2f_mult(vector2f v, float k);
+vector2i v2f_to_v2i(vector2f v);
 
 // v3i
 
-float v3i_length(struct vector3i v);
-struct vector3i v3i_add(struct vector3i v1, struct vector3i v2);
-struct vector3i v3i_mult(struct vector3i v, int k);
-struct vector3f v3i_to_v3f(struct vector3i v);
+float v3i_length(vector3i v);
+vector3i v3i_add(vector3i v1, vector3i v2);
+vector3i v3i_mult(vector3i v, int k);
+vector3f v3i_to_v3f(vector3i v);
 
 // v3f
 
-float v3f_length(struct vector3f v);
-struct vector3f v3f_add(struct vector3f v1, struct vector3f v2);
-struct vector3f v3f_mult(struct vector3f v, float k);
-struct vector3i v3f_to_v3i(struct vector3f v);
+float v3f_length(vector3f v);
+vector3f v3f_add(vector3f v1, vector3f v2);
+vector3f v3f_mult(vector3f v, float k);
+vector3i v3f_to_v3i(vector3f v);
 
 // v4i
 
-float v4i_length(struct vector4i v);
-struct vector4i v4i_add(struct vector4i v1, struct vector4i v2);
-struct vector4i v4i_mult(struct vector4i v, int k);
-struct vector4f v4i_to_v4f(struct vector4i v);
+float v4i_length(vector4i v);
+vector4i v4i_add(vector4i v1, vector4i v2);
+vector4i v4i_mult(vector4i v, int k);
+vector4f v4i_to_v4f(vector4i v);
 
 // v4f
 
-float v4f_length(struct vector4f v);
-struct vector4f v4f_add(struct vector4f v1, struct vector4f v2);
-struct vector4f v4f_mult(struct vector4f v, float k);
-struct vector4i v4f_to_v4i(struct vector4f v);
+float v4f_length(vector4f v);
+vector4f v4f_add(vector4f v1,vector4f v2);
+vector4f v4f_mult(vector4f v, float k);
+vector4i v4f_to_v4i(vector4f v);
